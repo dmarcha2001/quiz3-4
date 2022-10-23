cd8d394c9cdde477fdbe22a4ba77782761326e6d
dmarcha <dmarcha@cpp.edu>
2022-10-23 09:38:21 -0700

    init commit

---
+++ magicMain.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+// function that can test whether an array is a magic square or not:
+
+
+
+int main(){
+    // init the double array
+    // pass it to the function
+    // print if its a magic square or not
+    return 0;
+}
